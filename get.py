9a0b7e8dd4f2bf95c48fa5d86bd3f96d0d2eadff
ESeNonFossiIo <esenonfossiio@gmail.com>
2018-02-26 17:13:34 +0100

    Improve get method

--- get.py
+++ get.py
@@ -2,7 +2,10 @@ import json, requests
 
 url = 'http://localhost:8008'
 
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+headers = {
+    'content-type'   : 'application/json',
+    'Accept-Charset' : 'UTF-8'
+}
 
 resp = requests.get(url=url, headers=headers)
 data = json.loads(resp.text)
--- post.py
+++ post.py
@@ -3,7 +3,12 @@ import json
 
 url = 'http://localhost:8008'
 
-payload = json.dumps({'first': '1', 'second': '2'})
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+post_request_data = {
+    'data'      : json.dumps( {'first': '1', 'second': '2'} ),
+    'headers'   : {
+        'content-type'   : 'application/json',
+        'Accept-Charset' : 'UTF-8'
+    }
+}
 
-r = requests.post(url, data=payload, headers=headers)
+r = requests.post(url, **post_request_data)
