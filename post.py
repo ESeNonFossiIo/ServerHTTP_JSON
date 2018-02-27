f39ba03b99299b74ffc2006779f3c548536e8a37
ESeNonFossiIo <esenonfossiio@gmail.com>
2018-02-27 09:16:19 +0100

    Update code

--- post.py
+++ post.py
@@ -4,7 +4,7 @@ import json
 url = 'http://localhost:8008'
 
 post_request_data = {
-    'data'      : json.dumps( {'first': '1', 'second': '2'} ),
+    'data'      : json.dumps( {'data': 'new_text'} ),
     'headers'   : {
         'content-type'   : 'application/json',
         'Accept-Charset' : 'UTF-8'
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import json
 import cgi
 
 global data
-data = json.dumps({'hello': 'wooorld', 'received': 'ok2'})
+data = json.dumps({'data' : 'text'})
 
 class Server(BaseHTTPRequestHandler):
 
@@ -16,13 +16,11 @@ class Server(BaseHTTPRequestHandler):
     def do_HEAD(self):
         self._set_headers()
 
-    # GET sends back a Hello world message
     def do_GET(self):
         global data
         self._set_headers()
         self.wfile.write(data)
 
-    # POST echoes the message adding a JSON field
     def do_POST(self):
         global data
         ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
@@ -33,16 +31,9 @@ class Server(BaseHTTPRequestHandler):
             self.end_headers()
             return
 
-        # read the message and convert it into a python dictionary
         length = int(self.headers.getheader('content-length'))
         data = json.dumps(json.loads(self.rfile.read(length)))
 
-        # add a property to the object, just to mess with data
-        # data['received'] = 'ok'
-
-        # print data
-
-        # send the message back
         self._set_headers()
         self.wfile.write(data)
 
@@ -50,7 +41,7 @@ def run(server_class=HTTPServer, handler_class=Server, port=8008):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
 
-    print 'Starting httpd on port %d...' % port
+    print 'PORT %d...' % port
     httpd.serve_forever()
 
 if __name__ == "__main__":
